f1c7a1606fc8471d657f0cb2a8c18ef4256f8594
tangtang <2029229629@qq.com>
2022-05-30 20:26:53 +0800

    完成了主页功能

--- assets/js/baseapi.js
+++ assets/js/baseapi.js
@@ -1,3 +1,23 @@
+// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
+// 会先调用 ajaxPrefilter 这个函数
+// 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter((options)=>{
     options.url='http://www.liulongbin.top:3007'+options.url
+    if(options.url.includes('/my/')){
+        options.headers= {Authorization:localStorage.getItem('token')}
+    }
+    //每次发送请求回来校验token是否存在，或则是否过期 
+    options.complete=res=>{
+        if(res.responseJSON.status===1&&res.responseJSON.message==='身份认证失败！'){
+            localStorage.removeItem('token')
+            location.href='/login.html'
+        }
+    }
 })
+
+
+
+
+
+
+                                                
---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+function getUserInfo(){
+    $.ajax({
+        type:'GET',
+        url:'/my/userinfo',
+        // headers:{
+        //     Authorization:localStorage.getItem('token')
+        // },
+        success:(res)=>{
+            if(res.status!==0) return layer.msg('获取用户信息失败')
+            layer.msg('获取用户信息成功')
+            renderAvatar(res.data)
+        }
+    })
+}
+
+//渲染用户信息
+const renderAvatar=(user)=>{
+    console.log(user);
+    let uname=user.nickname||user.username
+    //渲染欢迎语
+    $('#welcome').html(`欢迎${uname}`)
+    //按需渲染头像
+    if(user.user_pic !==null){
+        //设置图片头像
+        $('.layui-nav-img').attr('src',user.user_pic)
+        $('.text-avatar').hide()
+    }else{
+        $('.layui-nav-img').hide()
+        $('.text-avatar').html(uname[0].toUpperCase())
+    }
+}
+//退出功能
+$('#del').click(()=>{
+    layer.confirm('是否退出？',{icon:3,title:'提示'},function(index){
+        localStorage.removeItem('token')
+        location.href='/login.html'
+    }) 
+})
+getUserInfo()
